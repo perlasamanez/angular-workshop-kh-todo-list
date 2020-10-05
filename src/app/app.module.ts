b6cd9043291f5ff44593fbcbd14a59c1fa9ea105
Perla Del Castillo <perlitadelcastillo@gmail.com>
2020-10-05 00:20:58 -0400

    [KH-Todo-List] Added the forms module and created an example for two way binding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -12,7 +13,8 @@ import { TodoDetailComponent } from './todo-detail/todo-detail.component';
     TodoDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
