b4b1708637aab2a852497e3890069c454306ab37
Perla Del Castillo <perlitadelcastillo@gmail.com>
2020-10-04 23:31:14 -0400

    [KH-Todo-List] Added new components to start project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-workshop-kh-todo-list';
+  title = 'Knight Hacks To-Do List App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailComponent
   ],
   imports: [
     BrowserModule
