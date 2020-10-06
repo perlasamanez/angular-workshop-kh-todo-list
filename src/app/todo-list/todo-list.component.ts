3cdd4eec385d47f9ebc40c6eda31668161b8d88a
Perla Del Castillo <perlitadelcastillo@gmail.com>
2020-10-05 20:15:44 -0400

    [KH-Todo-List] Added add and delete functions for the app.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -13,20 +13,47 @@ export class TodoListComponent implements OnInit {
   //todoItem = 'Get people to sign up to the event.';
 
   // 2nd Example of an item with an interface and how to display this object
-  todoItem: Item = {
-    id: 1,
-    description: 'Get people to sign up to the event.' 
-  };
+  // todoItem: Item = {
+  //   id: 1,
+  //   description: 'Get people to sign up to the event.' 
+  // };
 
-  // Array to hold all the to-do items
-  todoList = [];
+  // Array to hold all the to-do items if it's strings
+  //todoListItems: string[];
+  //todoItem: string = '';
 
-  constructor() { }
+  // Array of items (object)
+  todoListItems: Item[];
+  todoItem: Item;
+  // Id is just generated here for simple purposes - no relation
+  id: number;
 
-  ngOnInit(): void {
+  constructor() {
+   }
+
+  ngOnInit() {
+    // Initialize our values
+    this.todoListItems = [];
+    this.todoItem = {
+      id: 0,
+      description: ''
+    }
+    this.id = 0;
   }
 
   // Add function
+  addItem(){
+      this.todoListItems.push({
+        id: this.id++,
+        description: this.todoItem.description
+      });
+      // testing purposes to see that our array of items is being populated!
+      console.log(this.todoListItems)
+  }
 
   // Delete function
+  deleteItem(i: number) {
+    this.todoListItems.splice(i, 1);
+    console.log(this.todoListItems)
+  }
 }
